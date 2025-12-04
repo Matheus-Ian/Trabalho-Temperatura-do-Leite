91d3c6be0c93f43c84b69a1a1591ef5e6aa97946
Matheus-Ian <121767679+Matheus-Ian@users.noreply.github.com>
2025-12-04 18:58:37 -0300

    Codigo
    Integrate DS18B20 temperature sensor with OLED display for real-time monitoring and alerts.

--- monitoramento.py
+++ monitoramento.py
@@ -1,11 +1,58 @@
-# Exemplo de leitura de temperatura simulada
-
+import machine
+import onewire
+import ds18x20
 import time
-import random
+from machine import Pin, I2C
+from ssd1306 import SSD1306_I2C
 
-def ler_temperatura():
-    return round(20 + random.random()*5, 2)
+# Inicialização do OLED
+i2c = I2C(0, scl=Pin(22), sda=Pin(21))
+oled = SSD1306_I2C(128, 64, i2c)
+
+# Pinos dos componentes
+PIN_SENSOR = 4
+PIN_LED_OK = 15
+PIN_LED_ALERT = 2
+PIN_BUZZER = 13
+
+# Inicializando hardware
+dat = Pin(PIN_SENSOR)
+ds = ds18x20.DS18X20(onewire.OneWire(dat))
+roms = ds.scan()
+
+led_ok = Pin(PIN_LED_OK, Pin.OUT)
+led_alert = Pin(PIN_LED_ALERT, Pin.OUT)
+buzzer = Pin(PIN_BUZZER, Pin.OUT)
+
+TEMP_MAX = 4.0
+
+def alerta():
+    led_ok.value(0)
+    led_alert.value(1)
+    buzzer.value(1)
+    time.sleep(0.2)
+    buzzer.value(0)
+
+def temperatura_ok():
+    led_alert.value(0)
+    buzzer.value(0)
+    led_ok.value(1)
 
 while True:
-    print("Temperatura:", ler_temperatura(), "°C")
-    time.sleep(2)
+    ds.convert_temp()
+    time.sleep(0.8)
+    temp = ds.read_temp(roms[0])
+
+    print("Temperatura:", temp, "°C")
+
+    # Exibe no display
+    oled.fill(0)
+    oled.text("Temp:", 0, 0)
+    oled.text(str(temp) + " C", 0, 15)
+    oled.show()
+
+    # Verificação da temperatura
+    if temp > TEMP_MAX:
+        alerta()
+    else:
+        temperatura_ok()
